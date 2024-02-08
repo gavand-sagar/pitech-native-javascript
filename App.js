2c0c7b136a62cba1d2a61ea88d002c064dd3fbc4
gavand-sagar <gavand.sagar.s@gmail.com>
2024-02-08 15:17:38 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+function LowerCaseText(props){
+  return (
+    <Text>{props.text.toLowerCase()}</Text>
+  );
+}
+
+function Rating(props){
+
+  let str = ""
+
+  for (let index = 0; index < props.value; index++) {
+    str+= "❤️"    
+  }
+
+
+  return (
+    <Text>{str}</Text>
+  )
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LowerCaseText text={"SAGAR"}/>
+      <LowerCaseText text={"Amit"}/>
+      <LowerCaseText text={"Jay"}/>
+
+      <Rating value={3}/>
+      <Rating value={2}/>
+      <Rating value={5}/>
+
     </View>
   );
 }
